5a18dc6c1de1671cd661685280651271f494a694
Thammarat Thamma <thammarat.th@up.ac.th>
2024-10-23 12:19:35 +0700

    finish modern frontend

--- ch-06/angular.js
+++ ch-06/angular.js
@@ -1,10 +1,10 @@
 var app = angular.module("app", []);
 
 app.component("rootComponent", {
-  template: '<p style="color:{{textColor}}">{{message}}</p><button ng-click="count=count+1">Increment</button><p>Count: {{count }}</p>',
-  controller: function($scope) {
-    $scope.message = "I am an Angular app! There are many like it, but this one is mine!!";
-    $scope.count = 0;
-    $scope.textColor = "red";
-  }
+    template: '<p style="color:{{textColor}}">{{message}}</p><button ng-click="count=count+1">เพิ่มค่า</button><p>จำนวน: {{count}}</p>',
+    controller: function($scope) {
+        $scope.message = "ฉันเป็นแอพ Angular! มีหลายแอพเหมือนฉัน แต่แอพนี้เป็นของฉัน!!";
+        $scope.count = 0;
+        $scope.textColor = "red";
+    }
 });
