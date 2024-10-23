20753e09dc36026138c8d9e6b1ad43d08aab56f3
Thammarat Thamma <thammarat.th@up.ac.th>
2024-10-23 16:38:21 +0700

    finish ch06

--- ch-06/typescript/example.ts
+++ ch-06/typescript/example.ts
@@ -6,10 +6,11 @@ class Person {
     this.age = inAge;
   }
   speak(wisdom: string) {
-    console.log(`Hello! My name is ${this.name} and I am ${this.age} years old. A bit of wisdom for you: ${wisdom}.`);
+    // console.log(`Hello! My name is ${this.name} and I am ${this.age} years old. A bit of wisdom for you: ${wisdom}.`);
+    console.log(`สวัสดี! ฉันชื่อ ${this.name} และฉันอายุ ${this.age} ปี ข้อคิดสำหรับคุณ: ${wisdom}.`);
   }
 }
 
 let p: Person = new Person("Xander", 36);
-p.speak("Man has not evolved an inch from the primordial slime that spawned him");
+p.speak("มนุษย์ไม่ได้วิวัฒนาการไปจากเมือกตะกอนดั้งเดิมที่ก่อกำเนิดเขาแม้แต่นิ้วเดียว");
 //p = "abc";
